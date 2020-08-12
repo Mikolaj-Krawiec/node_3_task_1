e408dbfb797581518b1ed1d179e4878d816b48d6
Mikolaj-Krawiec <59422679+Mikolaj-Krawiec@users.noreply.github.com>
2020-08-12 13:55:04 +0200

    Add WebSocket

--- client/app.js
+++ client/app.js
@@ -7,40 +7,47 @@ const messageContentInput = document.querySelector('#message-content');
 
 let userName;
 
-const login = event => {
-    event.preventDefault();
-    if(!userNameInput.value){
-        alert('Please type your name first');
-    } else {
-        userName = userNameInput.value;
-        loginForm.classList.remove('show');
-        messagesSection.classList.add('show');
-    }
-}
+const socket = io();
+socket.on('message', (event) => addMessage(event.author, event.content));
 
-const sendMessage = event => {
-    event.preventDefault();
-    if(!messageContentInput.value){
-        alert('Please type your message first');
-    } else {
-        addMessage(userName, messageContentInput.value)
-        messageContentInput.value = '';
-    }
-}
+const login = (event) => {
+  event.preventDefault();
+  if (!userNameInput.value) {
+    alert('Please type your name first');
+  } else {
+    userName = userNameInput.value;
+    socket.emit('login', { name: userName });
+    loginForm.classList.remove('show');
+    messagesSection.classList.add('show');
+  }
+};
+
+const sendMessage = (event) => {
+  event.preventDefault();
+  let messageContent = messageContentInput.value;
+  if (!messageContent.length) {
+    alert('You have to type something!');
+  } else {
+    addMessage(userName, messageContent);
+    socket.emit('message', { author: userName, content: messageContent });
+    messageContentInput.value = '';
+  }
+};
 
 function addMessage(author, content) {
-    const message = document.createElement('li');
-    message.classList.add('message');
-    message.classList.add('message--received');
-    if(author === userName) message.classList.add('message--self');
-    message.innerHTML = `
-      <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+  if (author === userName) message.classList.add('message--self');
+  if (author === 'Chat Bot') message.classList.add('message--bot');
+  message.innerHTML = `
+      <h3 class="message__author">${userName === author ? 'You' : author}</h3>
       <div class="message__content">
         ${content}
       </div>
     `;
-    messagesList.appendChild(message);
-  }
+  messagesList.appendChild(message);
+}
 
 loginForm.addEventListener('submit', login);
 addMessageForm.addEventListener('submit', sendMessage);
--- server.js
+++ server.js
@@ -1,15 +1,50 @@
 const express = require('express');
 const path = require('path');
+const socket = require('socket.io');
+
 const app = express();
 
 const messages = [];
+let users = [];
 
 app.use(express.static('client'));
 
 app.use('/*/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
 
-app.listen('8000', () => {
-    console.log('server running on port 8000');
-})
+const server = app.listen(8000, () => {
+  console.log('Server is running on Port:', 8000);
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+  socket.on('message', (message) => {
+    console.log("Oh, I've got something from " + socket.id);
+    messages.push(message);
+    socket.broadcast.emit('message', message);
+  });
+  socket.on('login', (payload) => {
+    users.push({
+      id: socket.id,
+      name: payload.name,
+    });
+    socket.broadcast.emit('message', {
+      author: 'Chat Bot',
+      content: `${payload.name} has joined the conversation!`,
+    });
+  });
+  socket.on('disconnect', () => {
+    const user = users.find(user => user.id == socket.id);
+    if(user) {
+        users = users.filter((user) => user.id != socket.id);
+        socket.broadcast.emit('message', {
+          author: 'Chat Bot',
+          content: `${user.name} has left the conversation...`,
+        });
+    }
+    console.log('Oh, socket ' + socket.id + ' has left');
+  });
+});
